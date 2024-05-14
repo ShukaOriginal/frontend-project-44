5d564aa933923a940c99d9ae4c79a76b3294e7ae
Shuka <kuraga908@mail.ru>
2024-05-14 13:42:51 +0700

    test7

--- src/calc.js
+++ src/calc.js
@@ -2,26 +2,35 @@ import readlineSync from 'readline-sync';
 import randInt from './randomGenerated.js';
 import wrongAnswerF from './wrongAnswer.js';
 
+function preparation() {
+  const number1 = randInt(100);
+  const number2 = randInt(100);
+  const sign = randInt(3) % 3;
+  let correctAnswer = 0;
+  let signChar = '';
+  if (sign === 0) {
+    correctAnswer = number1 + number2;
+    signChar = '+';
+  } else if (sign === 1) {
+    correctAnswer = number1 - number2;
+    signChar = '-';
+  } else if (sign === 2) {
+    correctAnswer = number1 * number2;
+    signChar = '*';
+  }
+  return [number1, number2, signChar, correctAnswer];
+}
+
 const calcGame = (username) => {
   console.log('What is the result of the expression?');
   let winScore = 0;
 
   for (let i = 0; i < 3; i += 1) {
-    const number1 = randInt(100);
-    const number2 = randInt(100);
-    const sign = randInt(3) % 3;
-    let correctAnswer = 0;
-    let signChar = '';
-    if (sign === 0) {
-      correctAnswer = number1 + number2;
-      signChar = '+';
-    } else if (sign === 1) {
-      correctAnswer = number1 - number2;
-      signChar = '-';
-    } else if (sign === 2) {
-      correctAnswer = number1 * number2;
-      signChar = '*';
-    }
+    const massiv = preparation();
+    const number1 = massiv[0];
+    const number2 = massiv[1];
+    const signChar = massiv[2];
+    const correctAnswer = massiv[3];
 
     console.log(`Question: ${number1} ${signChar} ${number2}`);
     const answer = readlineSync.question('Your answer: ');
