ff3ee818875dacccf41c77e3b4c6d212f0757f27
Shuka <kuraga908@mail.ru>
2024-05-14 13:26:24 +0700

    test6

--- src/wrongAnswer.js
+++ src/wrongAnswer.js
@@ -2,5 +2,4 @@ export default (answer, correctAnswer, name) => {
   // prettier-ignore
   console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
   console.log(`Let's try again, ${name}!`);
-  return;
 };
